5eb9799778a6682bbe2295c0d61c8d7000d3fe5d
Marek Koseski <marekkoseski@hype4mobile.com>
2022-11-13 12:12:28 +0100

    added project settings

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "eslint-config-prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
--- content-script/main.ts
+++ content-script/main.ts
@@ -7,80 +7,82 @@ export const WORDS = "words";
 let active: boolean | null;
 
 const getAllLinks = () => {
-  return Array.from(document.querySelectorAll('a'));
-}
+  return Array.from(document.querySelectorAll("a"));
+};
 
 const getLinksByWord = (word: string) => {
   const allLinks = getAllLinks();
-  return allLinks.filter(el => {
+  return allLinks.filter((el) => {
     const textContent = el.textContent;
     if (textContent) {
       return textContent.toLowerCase().includes(word);
-    };
+    }
   });
-}
+};
 
 const getLinksByWordsArray = (words: string[]) => {
   const allLinks = getAllLinks();
-  return allLinks.filter(el => {
-      const textContent = el.textContent?.toLowerCase();
-      if (textContent) {
-        return words.find((word) => textContent.includes(word));
-      }
+  return allLinks.filter((el) => {
+    const textContent = el.textContent?.toLowerCase();
+    if (textContent) {
+      return words.find((word) => textContent.includes(word));
+    }
   });
-}
+};
 
 const hideLinks = (links: HTMLAnchorElement[]) => {
   links.forEach((link) => {
-    link.style.visibility = 'hidden';
+    link.style.visibility = "hidden";
   });
-}
+};
 
 function showLinks(links: HTMLAnchorElement[]) {
   links.forEach((link) => {
-    link.style.visibility = 'visible';
+    link.style.visibility = "visible";
   });
 }
 
 function handleRequest(request: any) {
-  const {type, data} = request;
+  const { type, data } = request;
 
   switch (type) {
-    case ADD:
-      const {newWord} = data;
+    case ADD: {
+      const { newWord } = data;
       const newLinks = getLinksByWord(newWord);
 
       if (active) {
         showLinks(newLinks);
       }
       break;
-    case REMOVE:
-      const {removeWord} = data;
+    }
+    case REMOVE: {
+      const { removeWord } = data;
       if (active) {
         const links = getLinksByWord(removeWord);
         hideLinks(links);
       }
       break;
-    case ENABLE:
-      const {wordsToHide} = data;
+    }
+    case ENABLE: {
+      const { wordsToHide } = data;
       const linksToHide = getLinksByWordsArray(wordsToHide);
       hideLinks(linksToHide);
       active = true;
       break;
-    case DISABLE:
-      const {wordsToShow} = data;
+    }
+    case DISABLE: {
+      const { wordsToShow } = data;
       const linksToShow = getLinksByWordsArray(wordsToShow);
       showLinks(linksToShow);
       active = false;
       break;
+    }
     default:
       break;
   }
 }
 
-chrome.runtime.onMessage.addListener(
-  function(request) {
-    handleRequest(request);
-    return true;
-  }
-);
+chrome.runtime.onMessage.addListener(function (request) {
+  handleRequest(request);
+  return true;
+});
